e3a0f20ab3275f453ae2878ce75ac219967f53db
Dmytro Milinevskyy <milinevskyy@gmail.com>
2016-03-04 11:53:41 +0100

    properly include API header

--- judy64nb.c
+++ judy64nb.c
@@ -32,6 +32,8 @@
 #include <memory.h>
 #include <string.h>
 
+#include "judy64nb.h"
+
 #ifdef linux
 	#define _FILE_OFFSET_BITS 64
 	#define _LARGEFILE_SOURCE
@@ -51,47 +53,6 @@
 	#ifndef BIG_ENDIAN
 		#define BIG_ENDIAN 4321
 	#endif
-#endif
-
-typedef unsigned char uchar;
-typedef unsigned int uint;
-#define PRIuint			"u"
-
-#if defined(__LP64__) || \
-	defined(__x86_64__) || \
-	defined(__amd64__) || \
-	defined(_WIN64) || \
-	defined(__sparc64__) || \
-	defined(__arch64__) || \
-	defined(__powerpc64__) || \
-	defined (__s390x__)
-	//	defines for 64 bit
-
-	typedef unsigned long long judyvalue;
-	typedef unsigned long long JudySlot;
-	#define JUDY_key_mask (0x07)
-	#define JUDY_key_size 8
-	#define JUDY_slot_size 8
-	#define JUDY_span_bytes (3 * JUDY_key_size)
-	#define JUDY_span_equiv JUDY_2
-	#define JUDY_radix_equiv JUDY_8
-
-	#define PRIjudyvalue	"llu"
-
-#else
-	//	defines for 32 bit
-
-	typedef uint judyvalue;
-	typedef uint JudySlot;
-	#define JUDY_key_mask (0x03)
-	#define JUDY_key_size 4
-	#define JUDY_slot_size 4
-	#define JUDY_span_bytes (7 * JUDY_key_size)
-	#define JUDY_span_equiv JUDY_4
-	#define JUDY_radix_equiv JUDY_8
-
-	#define PRIjudyvalue	"u"
-
 #endif
 
 #define JUDY_mask (~(JudySlot)0x07)
@@ -130,27 +91,6 @@ judyvalue JudyMask[9] = {
 0xffffffffffULL, 0xffffffffffffULL, 0xffffffffffffffULL, 0xffffffffffffffffULL
 #endif
 };
-
-typedef struct {
-	void *seg;			// next used allocator
-	uint next;			// next available offset
-} JudySeg;
-
-typedef struct {
-	JudySlot next;		// judy object
-	uint off;			// offset within key
-	int slot;			// slot within object
-} JudyStack;
-
-typedef struct {
-	JudySlot root[1];	// root of judy array
-	void **reuse[8];	// reuse judy blocks
-	JudySeg *seg;		// current judy allocator
-	uint level;			// current height of stack
-	uint max;			// max height of stack
-	uint depth;			// number of Integers in a key, or zero for string keys
-	JudyStack stack[1];	// current cursor
-} Judy;
 
 #define JUDY_max	JUDY_32
 
